efbbb7029977a56e1a9206e83451e83241065610
Nanda Pellegrini <pellegrininanda96@gmail.com>
2025-01-23 12:57:32 +0100

    TimbraturaMapper

--- src/main/java/Project/GestioneAziendale/Mappers/TimbraturaMapper.java
+++ src/main/java/Project/GestioneAziendale/Mappers/TimbraturaMapper.java
@@ -1,6 +1,8 @@
 package Project.GestioneAziendale.Mappers;
 
 import Project.GestioneAziendale.Dtos.DipendenteDtos.DipendenteRequestRegister;
+import Project.GestioneAziendale.Dtos.TimbraturaDtos.TimbraturaRequestRegister;
+import Project.GestioneAziendale.Dtos.TimbraturaDtos.TimbraturaRequestUpdate;
 import Project.GestioneAziendale.Entities.Timbratura;
 import Project.GestioneAziendale.Services.DipendenteService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,29 @@ public class TimbraturaMapper
     @Autowired
     DipendenteService dipendenteService;
 
-    public Timbratura fromDipendenteRequestRegister(DipendenteRequestRegister dipendenteRequestRegister) {
+    public Timbratura fromTimbraturaRequestRegister(TimbraturaRequestRegister timbraturaRequestRegister)
+    {
+        return Timbratura
+                .builder()
+                .orario_entrata(timbraturaRequestRegister.orario_entrata())
+                .inizio_pranzo(timbraturaRequestRegister.inizio_pranzo())
+                .fine_pranzo(timbraturaRequestRegister.fine_pranzo())
+                .uscita(timbraturaRequestRegister.uscita())
+                .data_corrente(timbraturaRequestRegister.data_corrente())
+                .build();
+
+    }
+
+    public Timbratura fromTimbraturaRequestUpdate(TimbraturaRequestUpdate timbraturaRequestUpdate)
+    {
+        return Timbratura
+                .builder()
+                .orario_entrata(timbraturaRequestUpdate.Orario_entrata())
+                .inizio_pranzo(timbraturaRequestUpdate.Inizio_pranzo())
+                .fine_pranzo(timbraturaRequestUpdate.Fine_pranzo())
+                .uscita(timbraturaRequestUpdate.Uscita())
+                .data_corrente(timbraturaRequestUpdate.data_corrente())
+                .build();
 
     }
 }
