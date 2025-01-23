2ddd46d1703d533dd74613ab900d33fa0e6f3b73
Rafael98-stack <rafaelcasapaojr@gmail.com>
2025-01-23 12:28:36 +0100

    Created missing controllers without structure, still developing

---
+++ src/main/java/Project/GestioneAziendale/Controllers/ComunicazioneAziendaleController.java
@@ -0,0 +1,10 @@
+package Project.GestioneAziendale.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app/v1/ComunicazioneAziendale")
+public class ComunicazioneAziendaleController {
+
+}
---
+++ src/main/java/Project/GestioneAziendale/Controllers/TimbraturaController.java
@@ -0,0 +1,9 @@
+package Project.GestioneAziendale.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app/v1/Timbratura")
+public class TimbraturaController {
+}
